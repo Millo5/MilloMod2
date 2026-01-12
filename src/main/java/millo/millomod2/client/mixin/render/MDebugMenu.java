d9dc2e52a38d944977c880c5205e984758cde7e2
Millo <23leeuw@gmail.com>
2026-01-13 00:45:59 +0100

    fix null crash

--- src/main/java/millo/millomod2/client/mixin/render/MDebugMenu.java
+++ src/main/java/millo/millomod2/client/mixin/render/MDebugMenu.java
@@ -39,6 +39,7 @@ public class MDebugMenu {
         }
 
         Vec3d pos = HypercubeAPI.getPlotOrigin();
+        if (pos == null) return;
         String s = String.format(Locale.ROOT, "Plot XYZ: %.3f / %.5f / %.3f", pos.getX(), pos.getY(), pos.getZ());
         if (insertIndex >= 0) text.add(insertIndex+1, s);
         else text.add(s);
